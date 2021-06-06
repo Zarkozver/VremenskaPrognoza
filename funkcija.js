1a78400960714902477eeef230bca0ba35dd6b4b
Zarkozver <49551105+Zarkozver@users.noreply.github.com>
2021-06-06 22:57:20 +0200

    Add files via upload

---
+++ funkcija.js
@@ -0,0 +1,5 @@
+﻿function NovaProg() {
+    var l = document.getElementById("ddlist");
+    var sg = l.options[l.selectedIndex].value;
+    document.getElementById("prognoza").src = "https://naslovi.net/vremenska-prognoza/" + sg;
+}
